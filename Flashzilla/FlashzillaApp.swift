17951ff312dd3fd8b80829de812ab10c7c41799f
Artem Bolotov <teeemon@gmail.com>
2023-04-08 07:37:29 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by artembolotov on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
